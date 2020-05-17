f3c7d3b6735a630e5286857adedec59f05a05f1c
raazkarkee405 <raazkarkee405@gmail.com>
2020-05-17 11:25:14 +0545

    Task Entity is Added

--- app/src/main/java/com/example/todolistmvvm/tasks/MainActivity.java
+++ app/src/main/java/com/example/todolistmvvm/tasks/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.todolistmvvm;
+package com.example.todolistmvvm.tasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.todolistmvvm.R;
 
 public class MainActivity extends AppCompatActivity {
 
