7cb050dcdbe29749faa6124c47b2027b54817b9d
raazkarkee405 <raazkarkee405@gmail.com>
2020-05-17 16:17:32 +0545

    Task Adapter Added

---
+++ app/src/main/java/com/example/todolistmvvm/addedittask/AddEditTaskActivity.java
@@ -0,0 +1,145 @@
+package com.example.todolistmvvm.addedittask;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+
+import com.example.todolistmvvm.R;
+import com.example.todolistmvvm.database.AppDatabase;
+import com.example.todolistmvvm.database.TaskEntry;
+
+import java.util.Date;
+
+public class AddEditTaskActivity extends AppCompatActivity {
+
+    // Extra for the task ID to be recieved in the intent
+    public static final String EXTRA_TASK_ID = "extraTaskId";
+    // Extra for the task ID to be received after rotation
+    public static final String INSTANCE_TASK_ID = "instanceTaskId";
+    // Constants for priority
+    public static final int PRIORITY_HIGH = 1;
+    public static final int PRIORITY_MEDIUM = 2;
+    public static final int PRIORITY_LOW = 3;
+    // Constant for default task id to be used when not in update mode
+    private static final int DEFAULT_TASK_ID = -1;
+    // Constant for logging
+    private static final String TAG = AddEditTaskActivity.class.getSimpleName();
+    // Fields for views
+    EditText editTextTitle;
+    EditText editTextDescription;
+    RadioGroup mRadioGroup;
+    Button mButton;
+
+    private int mTaskId = DEFAULT_TASK_ID;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_task);
+
+        initViews();
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(INSTANCE_TASK_ID)) {
+            mTaskId = savedInstanceState.getInt(INSTANCE_TASK_ID, DEFAULT_TASK_ID);
+        }
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(EXTRA_TASK_ID)) {
+            mButton.setText(R.string.update_button);
+            if (mTaskId == DEFAULT_TASK_ID) {
+                // populate the UI
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(INSTANCE_TASK_ID, mTaskId);
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * initViews is called from onCreate to init the member variable views
+     */
+    private void initViews() {
+        editTextTitle = findViewById(R.id.editTextTaskTitle);
+        editTextDescription = findViewById(R.id.editTextTaskDescription);
+        mRadioGroup = findViewById(R.id.radioGroup);
+        mButton = findViewById(R.id.saveButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSaveButtonClicked();
+            }
+        });
+    }
+
+    /**
+     * populateUI would be called to populate the UI when in update mode
+     *
+     * @param task the taskEntry to populate the UI
+     */
+
+    private void populatedUI(TaskEntry task) {
+
+    }
+
+    public void onSaveButtonClicked() {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        int priority = getPriorityFromViews();
+        Date date = new Date();
+        final TaskEntry task = new TaskEntry(title, description, priority, date);
+        AppDatabase.databaseWriteExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                AppDatabase.getInstance(getApplicationContext()).taskDao().insertTask(task);
+            }
+        });
+
+        finish();
+    }
+
+    /**
+     * getPriority is called whenever the selected priority needs to be retrieved
+     */
+    public int getPriorityFromViews() {
+        int priority = 1;
+        int checkedId = ((RadioGroup) findViewById(R.id.radioGroup)).getCheckedRadioButtonId();
+        switch (checkedId) {
+            case R.id.radButton1:
+                priority = PRIORITY_HIGH;
+                break;
+            case R.id.radButton2:
+                priority = PRIORITY_MEDIUM;
+                break;
+            case R.id.radButton3:
+                priority = PRIORITY_LOW;
+        }
+        return priority;
+    }
+
+    /**
+     * setPriority is called when we receive a task from MainActivity
+     *
+     * @param priority the priority value
+     */
+    public void setPriorityInViews(int priority) {
+        switch (priority) {
+            case PRIORITY_HIGH:
+                ((RadioGroup) findViewById(R.id.radioGroup)).check(R.id.radButton1);
+                break;
+            case PRIORITY_MEDIUM:
+                ((RadioGroup) findViewById(R.id.radioGroup)).check(R.id.radButton2);
+                break;
+            case PRIORITY_LOW:
+                ((RadioGroup) findViewById(R.id.radioGroup)).check(R.id.radButton3);
+        }
+    }
+}
